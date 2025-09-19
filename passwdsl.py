8f579703057435818c0820941e8bf457f0ac021c
Coder'Phantom95 <159680854+CoderRony955@users.noreply.github.com>
2025-09-19 22:27:12 +0530

    main

--- passwdsl.py
+++ passwdsl.py
@@ -1,25 +1,37 @@
 from rich.console import Console
 from about.welcome import welcome
 from about.commands import all_commands
+import handlers
 
 console = Console()
 
 
 def main():
-    welcome()  # Show welcome message
+    try:
+        welcome()  # Show welcome message first time
 
-    while True:
-        user = input(">_ ")
-        if user == "help" or user == "h":
-            all_commands()
-        elif user == "":
-            continue
-        elif user == "q" or user == "exit" or user == "quit":
-            console.print("[bold italic]see ya![/bold italic]")
-            break
-        else:
-            console.print(
-                "\n[bold red]Unknown command![/bold red] Use 'help' to see all available commands\n")
+        while True:
+            try:
+                user = input(">_ ")
+                if user == "help" or user == "h":
+                    all_commands()
+                elif user.split()[0] == "passadd" and "-cred" in user.split() and "-of" in user.split(): 
+                    handlers.handle_commands(usercmd=user)
+                elif user == "passwds":
+                    handlers.handle_commands(usercmd=user)
+                elif user.split()[0] == "passwd" and "-platform" in user.split():
+                    handlers.handle_commands(usercmd=user)
+                elif user == "q" or user == "exit" or user == "quit":
+                    console.print("[bold italic]see ya![/bold italic]")
+                    break
+                else:
+                    console.print(
+                        "\n[bold red]Wrong command![/bold red] Use 'help' to see all available commands\n")
+            except Exception:
+                continue
+
+    except KeyboardInterrupt:
+        console.print("[bold italic]see ya![/bold italic]")
 
 
 if __name__ == "__main__":
