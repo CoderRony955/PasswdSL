114e60f2f5520a96b80349c2492c15b96e7456eb
Coder'Phantom95 <159680854+CoderRony955@users.noreply.github.com>
2025-09-21 20:11:09 +0530

    update password

---
+++ operations/updatepass.py
@@ -0,0 +1,40 @@
+from db.dboperation import Operation
+from rich.console import Console
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+console = Console()
+
+
+def update_credential(platform_name: str, passwd: str):
+    """display all credentials 
+    """
+    dbname = os.getenv('DBNAME')
+    host = os.getenv('HOST')
+    port = os.getenv('PORT')
+    user = os.getenv('USER')
+    password = os.getenv('PASSWORD')
+    try:
+        with Operation(dbname=dbname, host=host, user=user, password=password, port=port) as cur:
+            cur.execute(
+                "UPDATE public.allpasswds SET passwd = %s WHERE platform = %s RETURNING id", (passwd, platform_name,))
+
+            result = cur.fetchone()
+            if not result:  # If data does not exist
+                console.print(
+                    f"[red]There is no password detected in database with platform name[/red] '{platform_name}'\n")
+                return
+
+            console.print(
+                f"[bold cyan]Password updation for platform[/bold cyan] '{platform_name}'")
+            console.print(
+                f"[bold blue]Password of[/bold blue] '{platform_name}' [bold blue]has been successfully updated![/bold blue]")
+            print()
+    except Exception:
+        console.print("""
+Failed to display your credentials, it may be cause by following reasons:
+1. You did not created table in your database.
+2. PostgreSQL service is not running.
+3. Database does not exist.\n
+""")
