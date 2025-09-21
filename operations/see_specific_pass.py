a7ae3c91f3d34b406efffc3e335211708ff7d977
Coder'Phantom95 <159680854+CoderRony955@users.noreply.github.com>
2025-09-21 20:08:41 +0530

    see specific password

--- operations/see_specific_pass.py
+++ operations/see_specific_pass.py
@@ -17,14 +17,24 @@ def display_specific_credential(platform_name: str):
     password = os.getenv('PASSWORD')
     try:
         with Operation(dbname=dbname, host=host, user=user, password=password, port=port) as cur:
-            cur.execute("SELECT * FROM public.allpasswds WHERE platform = %s", (platform_name,))
-            console.print(cur.fetchone())
+            cur.execute(
+                "SELECT * FROM public.allpasswds WHERE platform = %s", (platform_name,))
+            
+            result = cur.fetchone()
+            if not result: # If data does not exist 
+                console.print(f"[red]There is no password detected in database with platform name[/red] '{platform_name}'\n")
+                return
+            
+            console.print(f"""
+[bold]id:[/bold] {result[0]}
+[bold]platform:[/bold] {result[1]}
+[bold]password:[/bold] [cyan]{result[2]}[/cyan]
+                          """)
             print()
-    except Exception as e:
-
-        console.print(e)
+    except Exception:
         console.print("""
 Failed to display your credentials, it may be cause by following reasons:
 1. You did not created table in your database.
-2. Wrong command syntax.\n
-                      """)
+2. PostgreSQL service is not running.
+3. Database does not exist.\n
+""")
