9d45892290659c3fbbdd1f01d1dbf17e64412514
Coder'Phantom95 <159680854+CoderRony955@users.noreply.github.com>
2025-09-21 20:10:57 +0530

    see all passwords

--- operations/seeallpasswds.py
+++ operations/seeallpasswds.py
@@ -1,20 +1,12 @@
 from db.dboperation import Operation
 from rich.console import Console
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
 console = Console()
 
 
-def display_credentials():
+def display_credentials(dbname, host, user, password, port):
     """display all credentials 
     """
-    dbname = os.getenv('DBNAME')
-    host = os.getenv('HOST')
-    port = os.getenv('PORT')
-    user = os.getenv('USER')
-    password = os.getenv('PASSWORD')
     try:
         with Operation(dbname=dbname, host=host, user=user, password=password, port=port) as cur:
             cur.execute("SELECT * FROM public.allpasswds")
@@ -22,11 +14,9 @@ def display_credentials():
                 console.print(
                     f"{creds}")
             print()
-    except Exception as e:
-
-        console.print(e)
+    except Exception:
         console.print("""
 Failed to display your credentials, it may be cause by following reasons:
 1. You did not created table in your database.
-2. Wrong command syntax.\n
-                      """)
+2. PostgreSQL service is not running.\n
+""")
